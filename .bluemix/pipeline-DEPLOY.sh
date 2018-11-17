03d3312a366c50167ff6adfb50f43dfff9b9367c
root <root@ogre-js1.rtp.raleigh.ibm.com>
2018-11-17 05:22:20 -0500

    Automated Update

--- .bluemix/pipeline-DEPLOY.sh
+++ .bluemix/pipeline-DEPLOY.sh
@@ -41,36 +41,7 @@ figlet 'IBM Cloud'
 
 echo 'Retrieving Cloud Functions authorization key...'
 
-# Retrieve the Functions authorization key
-CF_ACCESS_TOKEN=`echo $CF_CONFIG_JSON | jq -r .AccessToken | awk '{print $2}'`
-
-# Docker image should be set by the pipeline, use a default if not set
-if [ -z "$FUNCTIONS_API_HOST" ]; then
-  echo 'FUNCTIONS_API_HOST was not set in the pipeline. Using default value.'
-  export FUNCTIONS_API_HOST=openwhisk.ng.bluemix.net
-fi
-FUNCTIONS_KEYS=`curl -XPOST -k -d "{ \"accessToken\" : \"$CF_ACCESS_TOKEN\", \"refreshToken\" : \"$CF_ACCESS_TOKEN\" }" \
-  -H 'Content-Type:application/json' https://$FUNCTIONS_API_HOST/bluemix/v2/authenticate`
-
-
-SPACE_KEY=`echo $FUNCTIONS_KEYS | jq -r '.namespaces[] | select(.name == "'$CF_ORG'_'$CF_SPACE'") | .key'`
-SPACE_UUID=`echo $FUNCTIONS_KEYS | jq -r '.namespaces[] | select(.name == "'$CF_ORG'_'$CF_SPACE'") | .uuid'`
-FUNCTIONS_AUTH=$SPACE_UUID:$SPACE_KEY
-
-BEARER_PREFIX="bearer "
-APIGW_ACCESS_TOKEN=${CF_ACCESS_TOKEN%.$BEARER_PREFIX}
-
-echo "writing .wskprops..."
-
-printf "APIVERSION=v1 \n\
-APIHOST=$FUNCTIONS_API_HOST \n\
-AUTH=$FUNCTIONS_AUTH \n\
-NAMESPACE=_ \n\
-APIGW_ACCESS_TOKEN=$APIGW_ACCESS_TOKEN" > $HOME/.wskprops
-
-
-
-
+ibmcloud fn api list
 
 ################################################################
 # Deploy 
